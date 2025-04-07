f843e43f3e5213d350f0492d0b99dd1f4bf09b89
Дмитрий <dimakol2005@mail.ru>
2025-04-07 16:09:01 +0500

    refactor preprocessing

--- my_pipeline/my_download.py
+++ my_pipeline/my_download.py
@@ -14,12 +14,15 @@ def preprocessing(path2df):
 
     columns_to_drop = [x for x in df.columns if df[x].isna().mean() > 0.15]
     columns_to_drop.append('Final_Exam_Score')
+    columns_to_drop.append('Student_ID')
     df.drop(columns=columns_to_drop, inplace=True)
 
-    for num_col in df.columns:
-        df[num_col] = df[num_col].fillna(df[num_col].median())
+    df['Pass_Fail'] = df['Pass_Fail'].apply(lambda x: 1 if x == 'pass' else 0)
 
-    df.to_csv('student_performance_clear.csv')
+    cat_columns = ['Gender', 'Parental_Education_Level', 'Internet_Access_at_Home', 'Extracurricular_Activities']
+    df_encoded = pd.get_dummies(df, columns=cat_columns, drop_first=True)
+
+    df_encoded.to_csv('student_performance_clear.csv')
     return True
 
 
